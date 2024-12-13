8a58a20baf793c9a60fbdd2929de64888f0ef37b
hieucaotrantrong <hieu98861@donga.edu.vn>
2024-12-13 23:43:13 +0700

    spring

---
+++ Downloads/Project_springtool-master (1)/Project_springtool-master/src/main/java/com/ck/controller/ProductController.java
@@ -0,0 +1,112 @@
+package com.ck.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.view.RedirectView;
+
+
+import com.ck.FileUploadUtil;
+import com.ck.model.Product;
+import com.ck.service.ProductService;
+
+@Controller
+public class ProductController {
+	@Autowired
+	private ProductService productService;
+	
+
+
+	@GetMapping("/")
+	public String viewHomePage(Model model) {
+		return findPaginated(1, "id", "asc", model);
+//		return"productPage";
+		
+	}
+
+// this is use in pagination and sorting@
+	@RequestMapping("index")
+	public String Project1(Model model, @Param("keyword") String keyword) {
+		List<Product> listProduct = productService.getAllProduct(keyword);
+		model.addAttribute("listProduct", listProduct);
+		model.addAttribute("keyword", keyword);
+		return "index";
+	}
+
+
+	  @GetMapping("/productPage")
+	    public String showProductPage(Model model) {
+	        // Bạn có thể thêm dữ liệu vào model nếu cần
+	        model.addAttribute("message", "Trang sản phẩm của chúng tôi!");
+	        return "productPage"; 
+	    }
+	@GetMapping("showNewProductForm")
+	public String Project2(Model model) {
+		// create model attribute to bind from data
+		Product product = new Product();
+		model.addAttribute("product", product);
+		return "Add_Product";
+
+	}
+
+
+	@PostMapping("/saveProduct")
+	public RedirectView saveProduct(@ModelAttribute("product") Product product,
+			@RequestParam("image") MultipartFile muiMultipartFile) throws IOException {
+		String fileName = StringUtils.cleanPath(muiMultipartFile.getOriginalFilename());
+		product.setPhotos(fileName);
+		Product saveProduct = productService.saveProduct(product);
+		String uploadDir = "product-photos/" + saveProduct.getId();
+		FileUploadUtil.SaveFile(uploadDir, fileName, muiMultipartFile);
+		return new RedirectView("/", true);
+	}
+
+	@GetMapping("showFormForUpdate/{id}")
+	public String UpdateImage(@PathVariable(value = "id") long id, Model model) {
+		// get employee from the service
+		Product product = productService.getProductById(id);
+		// set employee as model attribute to
+		model.addAttribute("product", product);
+		return "Edit_product";
+	}
+
+	@GetMapping("/deleteProduct/{id}")
+	public String deleteProduct(@PathVariable(value = "id") long id) {
+		// call delete employee method
+		this.productService.deleteProductByid(id);
+		return "redirect:/";
+	}
+
+
+	@GetMapping("/page/{pageNo}")
+	public String findPaginated(@PathVariable(value = "pageNo") int pageNo, 
+	                             @RequestParam("sortField") String sortField,
+	                             @RequestParam("sortDir") String sortDir, 
+	                             Model model) {
+	    int pageSize = 10;  // Tăng pageSize lên 10 (2 hàng)
+	    Page<Product> page = productService.findPaginated(pageNo, pageSize, sortField, sortDir);
+	    List<Product> listProduct = page.getContent();
+	    model.addAttribute("currentPage", pageNo);
+	    model.addAttribute("totalPages", page.getTotalPages());
+	    model.addAttribute("totalItems", page.getTotalElements());
+	    model.addAttribute("listProduct", listProduct);
+	    model.addAttribute("sortField", sortField);
+	    model.addAttribute("sortDir", sortDir);
+	    return "index";  // Hoặc tên trang của bạn
+	}
+
+
+}
